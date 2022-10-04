f60f73bf38fffd56272d112432580d0cbfb09c2c
unknown <neucothe9796@gmail.com>
2022-10-04 07:46:17 +0700

    first commit

--- src/admin/ManageList/ManageList.jsx
+++ src/admin/ManageList/ManageList.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './manage.css'
 import { Link } from 'react-router-dom'
 import FolderSpecialIcon from '@mui/icons-material/FolderSpecial';
+import axios from 'axios';
+
 
 const ManageList = () => {
   return (
