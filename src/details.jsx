91d196fddb0e7ab5f1138a91c12de666bf52dfb2
Sy997 <65949636+Sy997@users.noreply.github.com>
2022-10-07 22:43:50 +0700

    Create details.jsx

---
+++ src/details.jsx
@@ -0,0 +1,122 @@
+import React from "react"
+import Navbar from "../Navbar/Navbar"
+
+const Details = () => {
+  rerurn (
+    <div className="datails">
+    <Navbar />
+    <div className"tieccuois">
+      <div className="tieccuoi">
+        <img src="" alt="">
+        <span>Phòng vip</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>250.000.000Vnđ</p>
+          <p>169.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="tieccuoi">
+        <img src="" alt="">
+        <span>Phòng vip 2</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>230.000.000Vnđ</p>
+          <p>150.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="tieccuoi">
+        <img src="" alt="">
+        <span>Phòng trung bình</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>200.000.000Vnđ</p>
+          <p>130.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="tieccuoi">
+        <img src="" alt="">
+        <span>Phòng trung bình 2</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>200.000.000Vnđ</p>
+          <p>125.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="tieccuoi">
+        <img src="" alt="">
+        <span>Phòng thường</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>160.000.000Vnđ</p>
+          <p>100.000.000Vnđ</p>
+        </div>
+      </div>
+    </div>
+    <div className="sinhnhats">
+      <div className="sinhnhat">
+        <img src="" alt="">
+        <span>Phòng vip</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>50.000.000Vnđ</p>
+          <p>39.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="sinhnhat">
+        <img src="" alt="">
+        <span>Phòng vip 2</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>45.000.000Vnđ</p>
+          <p>35.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="sinhnhat">
+        <img src="" alt="">
+        <span>Phòng trung bình</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>40.000.000Vnđ</p>
+          <p>30.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="sinhnhat">
+        <img src="" alt="">
+        <span>Phòng thường</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>15.000.000Vnđ</p>
+          <p>6.000.000Vnđ</p>
+        </div>
+      </div>
+    </div>
+    <div className="giatiens">
+      <div className="giatien">
+        <img src="" alt="">
+        <span>Phòng Vip</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>15.000.000Vnđ</p>
+          <p>6.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="giatien">
+        <img src="" alt="">
+        <span>Phòng Vip 2</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>15.000.000Vnđ</p>
+          <p>6.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="giatien">
+        <img src="" alt="">
+        <span>Phòng trung bình</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>15.000.000Vnđ</p>
+          <p>6.000.000Vnđ</p>
+        </div>
+      </div>
+      <div className="giatien">
+        <img src="" alt="">
+        <span>Phòng thường</span>
+        <div className="price">
+          <p style={{textDecoration:"through-line"}}>15.000.000Vnđ</p>
+          <p>6.000.000Vnđ</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+  
+
+}
